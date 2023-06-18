c0f6b3baeecc6da8a0e5411bf2ef571e60f2e118
Sajan Kota <sajankota001@gmail.com>
2023-06-18 20:41:06 +0530

    remove unnecessary zip folders and added the configuration for Github pages deployment

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://sajankota.github.io
+# git push -f git@github.com:sajankota/sajankota.github.io.git main
+
+# if you are deploying to https://sajankota.github.io/3D-portfolio-react-app
+# git push -f git@github.com:sajankota/3D-portfolio-react-app.git main:gh-pages
+
+cd -
