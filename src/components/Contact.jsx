130566c4236762619081262308c320f6b3e8802c
Sajan Kota <sajankota001@gmail.com>
2023-06-18 16:10:59 +0530

    Added the Contact and Stars component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Sajan Kota",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "sajan.kota@sajan.kota.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
